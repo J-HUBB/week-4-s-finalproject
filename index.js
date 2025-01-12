a86369134ccd0a4f08c2a1def24b654f7a7f817c
J-HUBB <lowlifeenterprizes@yahoo.com>
2025-01-11 23:32:40 -0500

    week 4's final project

--- index.js
+++ index.js
@@ -1,29 +1,44 @@
-const searchBar = document.getElementById("searchBar");
-const results = document.getElementById("searchResults");
+
 
 async function onSearchChange(event) {
-  const fast = event.target.value;
-  const movies = await fetch(`https://www.omdbapi.com/?apikey=536c8bf5&s=fast`);
-  const moviesData = await movies.json();
-  const movieCardEl = document.querySelector(".movie-card");
-  movieCardEl.innerHTML = moviesData.map(movie => movieHTML(movie)).join("");
-}
 
-async function showSearchResults(fast) {
-  const movies = await fetch(`https://www.omdbapi.com/?apikey=536c8bf5&s=fast`);
-  const moviesData = await movies.json();
-  const movieCardEl = document.querySelector(".movie-card");
-  movieCardEl.innerHTML = moviesData.map(movie => movieHTML(movie)).join("");
-}
-
-function movieHTML(movie) {
-  `<div class="movie-card">
-        <div class="movie-card__container">
-        <h3>${movie.Title}</h4>
-        <p><b>${movie.Year}</b></p>
-        <p><b>${movie.Poster}</b></p>
-        </div>
-        </div>`;
-}
-
-onSearchChange(fast);
+    const query = event.target.value.trim();
+    
+    const movies = await fetch(`https://www.omdbapi.com/?apikey=536c8bf5&s=${query}`);
+    
+    const moviesData = await movies.json();
+    
+    console.log(moviesData.Search);
+    
+    if (moviesData.Response === "True") {
+    
+     results.innerHTML = moviesData.Search.map((movie) => movieHTML(movie)).join("");
+    
+     } else {
+    
+     results.innerHTML = <p>No results found for "${query}" </p>;
+    
+     
+    }
+}  
+    
+    
+    function movieHTML(movie) {
+    
+    return `<div class="movie-card">
+    
+     <div class="movie-card__container">
+    
+     <h3>${movie.Title}</h3>
+    
+     <p><b>Year:</b> ${movie.Year}</p>
+    
+     <img src="https://${movie.Poster}" alt="${movie.Title}" />
+    
+     </div>
+    
+     </div>`;
+    
+    }
+    
+    movieTitle.addEventListener("input", onSearchChange);
