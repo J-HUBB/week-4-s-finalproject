5a79bc0fe7ff04ad95ea317ce7785c01cb460c7b
J-HUBB <lowlifeenterprizes@yahoo.com>
2025-01-13 12:05:37 -0500

    week 4's final project

--- index.js
+++ index.js
@@ -1,39 +1,37 @@
+const movieTitle = document.getElementById('movieTitle')
 
 
 async function onSearchChange(event) {
-
     const query = event.target.value.trim();
-    
+    const movieResults = document.getElementById('movieResults')
     const movies = await fetch(`https://www.omdbapi.com/?apikey=536c8bf5&s=${query}`);
-    
     const moviesData = await movies.json();
-    
-    console.log(moviesData.Search);
     
     if (moviesData.Response === "True") {
     
-     results.innerHTML = moviesData.Search.map((movie) => movieHTML(movie)).join("");
+     movieResults.innerHTML = moviesData.Search.map((movie) => movieHTML(movie)).join("");
     
      } else {
     
-     results.innerHTML = <p>No results found for "${query}" </p>;
+     movieResults.innerHTML = <p>No results found for '${query}'</p>;
     
      
     }
-}  
-    
+}   
     
     function movieHTML(movie) {
     
-    return `<div class="movie-card">
+    return ` <div class="movie">
+    
+     <div class="movie-card">
     
      <div class="movie-card__container">
     
-     <h3>${movie.Title}</h3>
+     <h3>${moviesData.Search.Title}</h3>
     
-     <p><b>Year:</b> ${movie.Year}</p>
+     <p><b>Year:</b>${moviesData.Search.Year}</p>
     
-     <img src="https://${movie.Poster}" alt="${movie.Title}" />
+     <img src="${moviesData.Search.Poster}" alt="${moviesData.Search.Title}" />
     
      </div>
     
@@ -41,4 +39,8 @@ async function onSearchChange(event) {
     
     }
     
+    function getMovies() {
+        const query = document.getElementById('movieTitle').value.trim();
+        onSearchChange({ target: { value: query } });
+    }
     movieTitle.addEventListener("input", onSearchChange);
