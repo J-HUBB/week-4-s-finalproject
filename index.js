fe0cbb05c2b993536908e38a32a45721750a9dac
J-HUBB <lowlifeenterprizes@yahoo.com>
2025-01-26 13:33:58 -0500

    week 4's final project

--- index.js
+++ index.js
@@ -1,34 +1,40 @@
-const movieTitle = document.getElementById('movieTitle');
-const searchButton = document.querySelector('.search-wrapper');
-const searchResultText = document.querySelector('.search-info span:last-child');
-const loading = document.querySelector('.loading-state')
-
+const movieTitle = document.getElementById("movieTitle");
+const searchButton = document.querySelector(".search-wrapper");
+const searchResultText = document.querySelector(".search-info span:last-child");
+const loading = document.querySelector(".loading-state");
+const filterSection = document.getElementById("filter");
 
 async function onSearchChange(event) {
-    const query = event.target.value.trim();
-    const movieResults = document.getElementById('movieResults')
-    searchResultText.textContent = query;
-    loading.style.display = "flex";
-    const movies = await fetch(`https://www.omdbapi.com/?apikey=536c8bf5&s=${query}`);
-    const moviesData = await movies.json();
-    loading.style.display = "none";
-    
-    
-    if (moviesData.Response === "True") {
-    
-     movieResults.innerHTML = moviesData.Search.map((movie) => movieHTML(movie)).join("");
-    
-     } else {
-    
-     movieResults.innerHTML = `<span class="white"><p>No results found for : "${query}"</p></span>`;
-     
-     
-    }
-}   
-    
-    function movieHTML(movie) {
-    
-    return `<div class="movie-card">
+  const query = event.target.value.trim();
+  const movieResults = document.getElementById("movieResults");
+
+  if (!query) {
+    filterSection.style.display = "none";
+
+    return;
+  }
+
+  filterSection.style.display = "flex";
+
+  searchResultText.textContent = `"${query}"`;
+  loading.style.display = "flex";
+  const movies = await fetch(
+    `https://www.omdbapi.com/?apikey=536c8bf5&s=${query}`
+  );
+  const moviesData = await movies.json();
+  loading.style.display = "none";
+
+  if (moviesData.Response === "True") {
+    movieResults.innerHTML = moviesData.Search.map((movie) =>
+      movieHTML(movie)
+    ).join("");
+  } else {
+    movieResults.innerHTML = `<span class="white"><p>No results found for : "${query}"</p></span>`;
+  }
+}
+
+function movieHTML(movie) {
+  return `<div class="movie-card">
     
      <div class="movie-card__container">
     
@@ -41,24 +47,18 @@ async function onSearchChange(event) {
      </div>
     
      </div>`;
-    
-    }
-    
-   
+}
 
-    function getMovies() {
-        const query = document.getElementById('movieTitle').value.trim();
-        onSearchChange({ target: { value: query } });
-    }
+function getMovies() {
+  const query = document.getElementById("movieTitle").value.trim();
+  onSearchChange({ target: { value: query } });
+}
 
-    setTimeout(() => {
-        getMovies();
-    }, 2000);
 
-    movieTitle.addEventListener('keypress', function(event) {
-        if (event.key === 'Enter') {
-            getMovies();
-        }
-    });
+movieTitle.addEventListener("keypress", function (event) {
+  if (event.key === "Enter") {
+    getMovies();
+  }
+});
 
-    searchButton.addEventListener('click', getMovies);
+searchButton.addEventListener("click", getMovies);
